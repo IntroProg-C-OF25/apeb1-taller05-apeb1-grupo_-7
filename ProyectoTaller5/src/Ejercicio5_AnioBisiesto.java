8f4b21f54c785859453408be34ba9b83c72e326a
rjr16 <rickyrjrt@gmail.com>
2024-11-07 12:50:33 -0500

    Nueva actualizacion

--- ProyectoTaller5/src/Ejercicio5_AnioBisiesto.java
+++ ProyectoTaller5/src/Ejercicio5_AnioBisiesto.java
@@ -1,21 +1,27 @@
 
 /**
  * Año Bisiesto: Comprobar si un año ingresado es bisiesto o no. Si es bisiesto, mostrar un mensaje especial.
+ *
  * @author Smart
  */
 import java.util.Scanner;
+
 public class Ejercicio5_AnioBisiesto {
+
     public static void main(String[] args) {
-            int anio;
+        int anio;
         Scanner an = new Scanner(System.in);
-        
+
         System.out.print("Ingrese un anio: ");
         anio = an.nextInt();
-        
-        if (anio % 4==0) {
+
+        if (anio % 4 == 0) {
             System.out.print("El anio es bisiessto ");
         } else {
             System.out.print("El anio no es bisiesto ");
         }
     }
 }
+/*Ingrese un anio: 1666
+El anio no es bisiesto
+*/
